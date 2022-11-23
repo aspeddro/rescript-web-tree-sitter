1639c16db6f1cad57a64fc06889466bc3acbc2a4
Pedro Castro <aspeddro@gmail.com>
2022-11-23 11:12:07 -0300

    fix ci

--- scripts/wasm.mjs
+++ scripts/wasm.mjs
@@ -8,8 +8,8 @@ const __dirname = path.join(path.dirname(url.fileURLToPath(import.meta.url)), '.
 const artifact_dir = path.join(__dirname, folder)
 
 if (!fs.existsSync(artifact_dir)) {
-  child_process.exec(`mkdir -p ${artifact_dir}`)
-  child_process.exec(`git clone --depth 1 git@github.com:nkrkv/tree-sitter-rescript.git ${artifact_dir}`)
+  child_process.execSync(`mkdir -p ${artifact_dir}`)
+  child_process.execSync(`git clone --depth 1 git@github.com:nkrkv/tree-sitter-rescript.git ${artifact_dir}`)
 } else {
   console.warn(`Folder already exists`)
 }
@@ -24,19 +24,19 @@ console.log(`Changed dir to: ${process.cwd()}`)
 
 if (!fs.existsSync(`${path.join(process.cwd(), 'node_modules')}`)) {
   console.log(`Running yarn install`)
-  child_process.exec(`yarn`)
+  child_process.execSync(`yarn`)
 } else {
   console.warn('node_modules already exists')
 }
 
 const tree_sitter_cli = `${path.join(process.cwd(), 'node_modules', '.bin', 'tree-sitter')}`
 
-child_process.exec(`${tree_sitter_cli} generate`, (err, _, stderr) => {
+child_process.execSync(`${tree_sitter_cli} generate`, (err, _, stderr) => {
   if (err || stderr) {
     console.error('Failed to generate tree-sitter', stderr)
   }
 })
-child_process.exec(`${tree_sitter_cli} build-wasm`, (err, _, stderr) => {
+child_process.execSync(`${tree_sitter_cli} build-wasm`, (err, _, stderr) => {
   if (err || stderr) {
     console.error('Failed to build wasm', stderr)
   }
